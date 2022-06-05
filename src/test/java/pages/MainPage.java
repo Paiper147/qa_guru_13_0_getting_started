85a0a6d1ba4d4bfd424e8d5a2637f50373d4153c
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-06-05 23:20:23 +0700

    added Page Object pattern

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPage {
+    SelenideElement searchInput = $("[name=q]");
+
+    public MainPage openMainPage(String value){
+        open("https://www.google.com/");
+
+        return this;
+    }
+
+    //for successfulSearchTestWithNewPageObjectsTest()
+    public MainPage typeSearchMain(String value){
+        searchInput.setValue(value).pressEnter();
+
+        return this;
+    }
+    //for successfulSearchTestWithFluentTest()
+    public SearchPage typeSearchFluent(String value){
+        searchInput.setValue(value).pressEnter();
+
+        return new SearchPage();
+    }
+}
